5bcad96d29fe76fafabf8d39126c2dbef2c1e01b
yanyimin <993031727@qq.com>
2019-12-03 14:45:56 +0800

    Lambda 练习完毕

--- chapter_6_v1/src/main/java/com/mine/lambda/LambdaTest.java
+++ chapter_6_v1/src/main/java/com/mine/lambda/LambdaTest.java
@@ -66,6 +66,76 @@ public class LambdaTest {
         MyPrintf p = System.out::printf;
         p.printf("This is %s %s","my","printf");
     }
+
+
+
+    @Test
+    public void method9(){
+        Human<Person> h = Person::new;
+        System.out.println(h.nameAndPass("Frist","Last").toString());
+        Person p = new Person("王五","李四");
+        Human<String> h1 = p::getPerson;
+        System.out.println(h1.nameAndPass("张胜男","雷颜"));
+        Human<String> h2 = Person::get;
+        System.out.println(h2.nameAndPass("何鑫" ,"张琴"));
+        MyComparator mc = String::compareToIgnoreCase;
+        System.out.println(mc.comparing("aaaa","bbb"));
+        MyComparator mc1 =(x,y) -> x.compareToIgnoreCase(y);
+        MyComparator2 m2 = (x,y) -> x.test(y);
+        MyComparator2 m22 = MyTest::test;
+        System.out.println(m22.comparing(new MyTest(),"bbb"));
+    }
+}
+class MyTest{
+    int test(String a){
+        String  b = this.concat(a);
+        return b.length();
+    }
+    String concat(String a){
+        return this + a;
+    }
+}
+
+interface MyComparator{
+   int comparing(String s1,String s2);
+}
+interface MyComparator2{
+    int comparing(MyTest s1,String s2);
+}
+class Person{
+    private String fristname;
+    private String lastname;
+
+    public Person(String fristname, String lastname) {
+        this.fristname = fristname;
+        this.lastname = lastname;
+    }
+
+    public Person(String fristname) {
+        this.fristname = fristname;
+    }
+
+    /**
+     * lambda 调用object方法
+     * @param frist
+     * @param last
+     * @return
+     */
+    public String getPerson(String frist,String last){
+        return frist+last;
+    }
+
+    public static String get(String s1,String s2){
+        return s1 + " : " + s2;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fristname='" + fristname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
 
 interface MyPrint{
